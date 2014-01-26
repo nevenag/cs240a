fc251d8a170d8c010ec4897321356ba47898cf1a
choruk <chris@horukmail.com>
2014-01-25 18:30:43 -0800

    first attempt at calculating normB with log(p) communication time

--- cgsolve.c
+++ cgsolve.c
@@ -8,7 +8,8 @@
 #include "ddot.h"
 
 #define PROPORTIONALITY_CONSTANT 5
-#define DEBUG_1
+// #define DEBUG_1
+#define DEBUG_2
 
 void print_vector(double *vector, int size)
 {
@@ -91,13 +92,18 @@ void cgsolve_sequential(int k, double* result, double* norm, int* num_iter)
     memcpy(result, x, n*sizeof(double));
     return;
 }
+// Helper methods for cgsolve_parallel that implement the different routines
+// (i.e. different modes of operation in the main loop)
+void cgsolve_master_routine();
+void cgsolve_slave_routine();
 /**
  * This method should only get called if size > 1, i.e. more than one processor is available.
  * Per the problem description, this method also assumes that k is evenly divisible by size.
 **/
 void cgsolve_parallel(int k, int rank, int size, double* result, double* norm, int* num_iter)
 {
-    int vector_size = (k*k)/size, maxiters = PROPORTIONALITY_CONSTANT * k, iter_index = 0, i = 0;
+    int vector_size = (k*k)/size, maxiters = PROPORTIONALITY_CONSTANT * k, iter_index = 0, i = 0, tag = 0;
+    MPI_Status status;
     // Distributed vectors have only n/p elements each
     double x[vector_size], r[vector_size], r_new[vector_size], d[vector_size];
     // x = zeros(n, 1)
@@ -108,7 +114,72 @@ void cgsolve_parallel(int k, int rank, int size, double* result, double* norm, i
     // r = b
     for (i = 0; i < vector_size; i++)
     {
-        r[i] = cs240_getB(i, n);
+        r[i] = cs240_getB(i, k*k);
     }
-    
+    // calculate the ddot of our chunk of b and set relres to 1.0 (relres = norm(b-Ax)/norm(b) = norm(b)/norm(b), x=zeros(n, 1))
+    // Note that this is only the norm of the portion of b that we are responsible for
+    double btb = ddot(r, r, vector_size), relres = 1.0;
+    // Assumes size = 2^x
+    int log_p = log(size)/log(2);
+    // This is the master, who will collect all the results together
+    if (rank == 0)
+    {
+        // Need to aggregate the ddot(btb) results
+        for (i = 0; i < log_p; i++)
+        {
+            int src_rank = pow(2, i);
+            double message;
+            MPI_Recv(&message, 1, MPI_DOUBLE, src_rank, tag, MPI_COMM_WORLD, &status);
+            btb += message;
+        }
+        // Now calculate the norm of b
+        double normB = sqrt(btb);
+        MPI_Barrier(MPI_COMM_WORLD);
+#ifdef DEBUG_2
+        printf("normB = %f", normB);
+#endif
+        cgsolve_master_routine();
+    }
+    else
+    {
+        // Need to send the normB results to neigbors
+        for (i = 0; i < log_p; i++)
+        {
+            int increment = pow(2, i), rank_to_use = rank / increment;
+            double message;
+            if (rank_to_use % 2 == 0)
+            {
+                MPI_Recv(&message, 1, MPI_DOUBLE, rank + increment, tag, MPI_COMM_WORLD, &status);
+                btb += message;
+            }
+            else
+            {
+                MPI_Send(btb, 1, MPI_DOUBLE, rank - increment, tag, MPI_COMM_WORLD);
+                break;
+            }
+        }
+#ifdef DEBUG_2
+        printf("Process #%d waiting to synchronize...\n", rank);
+#endif
+        // Some processes break out of the above loop earlier than others, need to synchronize here
+        MPI_Barrier(MPI_COMM_WORLD);
+#ifdef DEBUG_2
+        printf("Process #%d synchronized.\n", rank)
+#endif
+        cgsolve_slave_routine();
+    }
+}
+/**
+ * The master routine for cgsolve_parallel. There is only one master.
+**/
+void cgsolve_master_routine()
+{
+    // TODO: Implement this.
+}
+/**
+ * The slave routine for cgsolve_parallel. Everyone is a slave besides the master.
+**/
+void cgsolve_slave_routine()
+{
+    // TODO: Implement this.
 }
