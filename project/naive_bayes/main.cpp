565e5509c07dc0a2772d8f7e5599bf2020c9c23b
choruk <chris@horukmail.com>
2014-02-27 13:38:44 -0800

    more precise timing code

--- project/naive_bayes/main.cpp
+++ project/naive_bayes/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <ctime>
 
 #include "naive_bayes.hpp"
 
@@ -7,14 +8,21 @@ using namespace std;
 int main()
 {
   cout << "Main" << endl;
-  
+  // Initialize the NB Classifier
   char categoryNamesFile[] = "category_names.txt";
-  // char vocabularyFile[] = "all-topics-strings.lc.txt";
-  NaiveBayesClassifier nbClassifier(categoryNamesFile/*, vocabularyFile*/);
+  NaiveBayesClassifier nbClassifier(categoryNamesFile);
   nbClassifier.printAllCategoryNames();
+  // How long does the training phase take?
+  clock_t start = clock();
   nbClassifier.learnFromTrainingSet();
+  clock_t runTime = clock() - start;
+  cout << "Elapsed time for learning: " << (double)runTime/CLOCKS_PER_SEC << " seconds" << endl;
+  // How quickly can we categorize a single docuemnt?
   char documentToClassify[] = "categorize.me";
+  start = clock();
   nbClassifier.classifyDocument(documentToClassify);
+  runTime = clock() - start;
+  cout << "Elapsed time for classification: " << (double)runTime/CLOCKS_PER_SEC << " seconds" << endl;
   
   return 0;
 }
