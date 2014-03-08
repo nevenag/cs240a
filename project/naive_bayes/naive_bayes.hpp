c8383770acb6c38f3ba936aa96777d1ff6b638d0
choruk <chris@horukmail.com>
2014-03-08 15:31:20 -0800

    update naive_bayes.hpp

--- project/naive_bayes/naive_bayes.hpp
+++ project/naive_bayes/naive_bayes.hpp
@@ -11,8 +11,11 @@ class NaiveBayesClassifier
     NaiveBayesClassifier(std::string);
     void printAllCategoryNames();
     void learnFromTrainingSet(std::string,std::string);
+    void learnFromTrainingSetParallel(std::string,std::string,int,int);
     void classifyTestSet(std::string,std::string);
     void classifyDocument(std::string);
+    CategoryProbabilities** getCategoryProbabilities() { return categoryProbabilities; };
+    int getCategoryCount() { return categoryCount; };
   private:
     // Private members
     int categoryCount;
@@ -20,6 +23,7 @@ class NaiveBayesClassifier
     // Private methods
     void readInputCategories(std::string,std::string*);
     void readInputVocabulary(std::string,std::string*);
+    int learnForCategory(std::string,std::string,std::string,std::string,int);
 };
 
 #endif
