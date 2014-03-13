84487f98792493e612f529941b22e3cea9995bca
Nevena Golubovic <nevena@cs.ucsb.edu>
2014-03-12 17:13:42 -0700

    use 20news prefix for test set

--- project/naive_bayes/script.sh
+++ project/naive_bayes/script.sh
@@ -6,11 +6,11 @@ else
 	DBS=$1
 	echo "user specified number of processors"
 fi
-LINES=$(wc -l twenty_news_groups_test.tsv)
+LINES=$(wc -l ./20news/twenty_news_groups_test.tsv)
 #echo $LINES/$DBS
 TOTALLINES=$(echo $LINES | awk 'BEGIN {FS=" "}{print $1}')
 LINEC=$(echo $(($TOTALLINES/$DBS)))
 echo $LINEC
-split twenty_news_groups_test.tsv -d -l $LINEC
+split ./20news/twenty_news_groups_test.tsv -d -l $LINEC
 ./naivebayes 1 1 $DBS
 rm x*
