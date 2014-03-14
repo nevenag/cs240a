bcfa719dbb247375e37646c76de2fcfdb9a92df9
UCSB Training Admin <ucsb-train14@tscc-login1.sdsc.edu>
2014-03-14 10:45:30 -0700

    update script to take 3 parameters

--- project/naive_bayes/script.sh
+++ project/naive_bayes/script.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-if [ "$1" = "" ]; then
+if [ "$3" = "" ]; then
 	DBS=$(nproc)
 	echo "no arguments - reading nproc"
 else
-	DBS=$1
+	DBS=$3
 	echo "user specified number of processors"
 fi
 LINES=$(wc -l ./20news/twenty_news_groups_test.tsv)
@@ -12,5 +12,5 @@ TOTALLINES=$(echo $LINES | awk 'BEGIN {FS=" "}{print $1}')
 LINEC=$(echo $(($TOTALLINES/$DBS)))
 echo $LINEC
 split ./20news/twenty_news_groups_test.tsv -d -l $LINEC
-./naivebayes 1 1 $DBS
+./naivebayes $1 $2 $DBS
 rm x*
