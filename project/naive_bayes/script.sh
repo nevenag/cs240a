40d5569d277b38fb6096fccf24877607642c04db
UCSB Training Admin <ucsb-train22@tscc-login2.sdsc.edu>
2014-03-17 21:33:59 -0700

    update script.sh for enron

--- project/naive_bayes/script.sh
+++ project/naive_bayes/script.sh
@@ -8,8 +8,10 @@ else
 fi
 if [ $1 = 1 ]; then
   LINES=$(wc -l ./20news/twenty_news_groups_test.tsv)
-else
+elif [ $1 = 2 ]; then
   LINES=$(wc -l ./reuters/test/mega_document)
+else
+  LINES=$(wc -l ./enron/test/mega_document)
 fi
 #echo $LINES/$DBS
 TOTALLINES=$(echo $LINES | awk 'BEGIN {FS=" "}{print $1}')
@@ -19,8 +21,10 @@ LINEC=$(echo $(($TOTALLINES/$DBS)))
 mkdir -p "parallel_test_files"
 if [ $1 = 1 ]; then
   split ./20news/twenty_news_groups_test.tsv -d -l $LINEC "parallel_test_files/"
-else
+elif [ $1 = 2 ]; then
   split ./reuters/test/mega_document -d -l $LINEC "parallel_test_files/"
+else
+  split ./enron/test/mega_document -d -l $LINEC "parallel_test_files/"
 fi
 COVEREDLINES=$(echo "$LINEC * $DBS" | bc)
 if [ $COVEREDLINES != $TOTALLINES ]; then
